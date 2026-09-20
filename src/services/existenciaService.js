3b2c0ec3a327ab8cf644c4abd6e90cc3a3f4de41
tudytuumo01-maker <tudytuumo01@gmail.com>
2026-09-20 10:00:17 -0500

    modulo informe de inventario existencia articulo

--- src/services/existenciaService.js
+++ src/services/existenciaService.js
@@ -4,5 +4,9 @@ export default {
   getExistencias(payload) {
     // payload: { idEmpresa, pagina, textoFiltro, BolsaEstado, idBodega }
     return http.post('/existencia/getexistencias', payload)
+  },
+    getExistenciasArticulo(payload) {
+    // payload: { idEmpresa, idArticulo }
+    return http.post('/existencia/getexistenciasarticulo', payload)
   }
 }
